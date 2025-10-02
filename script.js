3d2ac052a90678ae3e1881649fe7077ca01e2fa9
rubenszabo <ruben.szabo@hbsz.edu.hu>
2025-10-02 20:33:21 +0200

    update 6

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ function on() {
 function off() {
   document.getElementById("overlay").style.display = "none";
 }
+document.addEventListener("DOMContentLoaded", function () {
+  const cards = document.querySelectorAll('.card');
+  const imgOverlay = document.getElementById('imgOverlay');
+  const overlayImg = document.getElementById('overlayImg');
+
+  cards.forEach(card => {
+    card.addEventListener('click', function () {
+      const img = card.querySelector('img');
+      overlayImg.src = img.src;
+      imgOverlay.style.display = 'flex';
+    });
+  });
+
+  imgOverlay.addEventListener('click', function () {
+    imgOverlay.style.display = 'none';
+    overlayImg.src = '';
+  });
+});
